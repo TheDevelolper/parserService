bb3bc03ff69b1376391a3cc60afebd3d6688fdf1
dojones1 <dojones1@users.noreply.github.com>
2019-09-08 22:10:18 +0100

    Simplify testdata

--- docker_build.sh
+++ docker_build.sh
@@ -3,6 +3,6 @@ set -e
 docker stop cparser_service | true
 docker rm cparser_service | true
 docker build --tag parser_service:dev .
-docker run --name cparser_service -d -p 5000:5000 -it parser_service:dev
+docker run --name cparser_service -d -p 5000:5000 -it parser_service:dev ./parserService.py &
 sleep 1
 docker logs -f cparser_service
